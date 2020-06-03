500ceb957e367e4c2e0cc378c5468a4d7f7a0c6c
Dima <Pilipenkodmitriy1@gmail.com>
2020-06-03 22:48:33 +0200

    create props

--- src/Components/Profile/MyPosts/MyPosts.jsx
+++ src/Components/Profile/MyPosts/MyPosts.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
 const MyPosts = (props) => {
-    let posts = [
-        {id: 1, message:'Hi, how are you?', likesCount: 4 },
-        {id: 2, message:'It\'s my first post', likesCount: 52 },
-        {id: 3, message:'Grate', likesCount: 5 },
-        {id: 4, message:'Please', likesCount: 88 },
-    ]
-    let postsElements = posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
+    // let posts = [
+    //     {id: 1, message:'Hi, how are you?', likesCount: 4 },
+    //     {id: 2, message:'It\'s my first post', likesCount: 52 },
+    //     {id: 3, message:'Grate', likesCount: 5 },
+    //     {id: 4, message:'Please', likesCount: 88 },
+    // ]
+    let postsElements = props.posts.map(p => <Post message={p.message} likesCount={p.likesCount}/>);
     return (
         <div className={s.posts}>
             <h3>My posts</h3>
